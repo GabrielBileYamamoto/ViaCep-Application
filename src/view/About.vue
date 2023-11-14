79a25dd067fcad425f5dce413f7962775ad32c3b
Gabriel Bile Yamamoto <gabriel2bile@gmail.com>
2023-11-14 10:01:02 -0300

    Activity Finished

---
+++ src/view/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Sobre o App</h1>
+        <p>Desenvolvidor por Gabriel Bile Yamamoto</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
